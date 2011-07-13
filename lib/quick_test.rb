1fb52e6c9f91fbbd4c6ae040b453a79406feb5c1
Jamesho <jamesh@nulogy.com>
2011-07-13 14:19:34 -0400

    more renaming

--- lib/quick_test.rb
+++ lib/quick_test.rb
@@ -1,8 +1,9 @@
 require 'test/unit'
-require 'quick_test/test_runner'
+require 'quick_test/quick_test'
 require 'quick_test/experiment'
-require 'quick_test/result'
 require 'quick_test/parameter'
+require 'quick_test/result'
+require 'quick_test/test_runner'
 
 # TODO: run tests that failed the last time
 # TODO: figure out how to reload helpers
@@ -11,96 +12,11 @@ require 'quick_test/parameter'
 # TODO: reports2_test doesn't seem to work the second time around
 # TODO: get line number out
 module QuickTest
-
   class << self
-  attr_accessor :experiments
-  attr_accessor :current_experiment
-
-
-  def initialize args={}
-    self.experiments = []
-  end
-=begin
-  def run_func test_path
-    test_file = "test/functional/#{test_path}_test.rb"
-    load test_file
-    test_class = begin
-     "#{test_path.split('/').last.camelize}Test".constantize
-    rescue NameError
-      "#{test_path.camelize}Test".constantize
+    def start
+      QuickTest.new
     end
-
-    test_suite = Test::Unit::TestSuite.new(test_class.to_s)
-    test_names = test_class.suite.tests.collect do |t|
-        t.method_name
-    end.compact
-
-    test_names.each do |tn|
-      test_suite << test_class.new(tn)
-    end
-
-     begin
-      runner = QuickTestRunner.new(test_suite)
-      runner.start
-
-      result = Result.new(runner)
-      result.output_metrics
-
-      test_suite.tests.each {|t| puts "    #{t.method_name}"}
-
-    ensure
-    end
-    nil
   end
-=end
-  def run test_path_or_experiment_number=nil, keyword=nil
-  self.experiments = []
-    if test_path_or_experiment_number.is_a? Integer
-      # TODO: handle bad numbers
-      self.current_experiment = self.experiments[test_path_or_experiment_number]
-    elsif test_path_or_experiment_number
-      self.current_experiment = Experiment.new test_path_or_experiment_number, keyword
-      self.experiments << self.current_experiment
-    end
-
-    self.current_experiment.run
-    nil
-  end
-
-  def run_bad
-    self.current_experiment.run_faults
-    nil
-  end
-
-  def freeze
-    self.current_experiment.freeze_result
-    nil
-  end
-
-  def unfreeze
-    self.current_experiment.unfreeze_result
-    nil
-  end
-
-  def metrics number=nil
-    self.current_experiment.output_metrics number
-    nil
-  end
-
-  def list
-    if self.experiments.size == 0
-      puts "No experiments have been run."
-      return
-    end
-
-    header = "###" + " " + "Test".ljust(20) + " " + "Keyword".ljust(10) + " " + "Results"
-    puts header.color(:blue).bright
-    self.experiments.each_with_index do |exp, n|
-      puts "#{n.to_s.rjust(3)} #{exp}"
-    end
-    nil
-  end
-end
 end
 
 # monkey patching!
--- lib/quick_test/parameter.rb
+++ lib/quick_test/parameter.rb
@@ -1,49 +1,49 @@
 module QuickTest
-class Parameter
-  attr_accessor :test_path, :keyword
-  attr_accessor :test_class
+  class Parameter
+    attr_accessor :test_path, :keyword
+    attr_accessor :test_class
 
-  def initialize test_path, keyword
-    self.test_path = test_path
-    self.keyword = keyword
-  end
-
-  def test_file
-    "test/unit/#{self.test_path}_test.rb"
-  end
-
-  def load_test_class
-    load test_file
-    self.test_class = begin
-     "#{self.test_path.split('/').last.camelize}Test".constantize
-    rescue NameError
-      "#{self.test_path.camelize}Test".constantize
+    def initialize test_path, keyword
+      self.test_path = test_path
+      self.keyword = keyword
     end
-    self.test_class
-  end
 
-  def test_names
-    names = self.test_class.suite.tests.collect do |t|
-      if self.keyword.blank?
-        t.method_name
-      else
-        t.method_name if t.method_name.include?(self.keyword)
+    def test_file
+      "test/unit/#{self.test_path}_test.rb"
+    end
+
+    def load_test_class
+      load test_file
+      self.test_class = begin
+       "#{self.test_path.split('/').last.camelize}Test".constantize
+      rescue NameError
+        "#{self.test_path.camelize}Test".constantize
       end
-    end.compact
-  end
+      self.test_class
+    end
 
-  def valid?
-    true
-  end
+    def test_names
+      names = self.test_class.suite.tests.collect do |t|
+        if self.keyword.blank?
+          t.method_name
+        else
+          t.method_name if t.method_name.include?(self.keyword)
+        end
+      end.compact
+    end
 
-  def tests_filtered?
-    !self.keyword.blank?
-  end
+    def valid?
+      true
+    end
 
-  def to_s
-    desc = self.test_path.ljust(20) + " "
-    desc << (self.keyword.blank? ? "".ljust(10) : self.keyword.ljust(10))
-    desc
+    def tests_filtered?
+      !self.keyword.blank?
+    end
+
+    def to_s
+      desc = self.test_path.ljust(20) + " "
+      desc << (self.keyword.blank? ? "".ljust(10) : self.keyword.ljust(10))
+      desc
+    end
   end
-end
 end
